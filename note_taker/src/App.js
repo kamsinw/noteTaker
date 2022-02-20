4343c91c2e03fb299d1819be59910477c0a3275e
kam1202 <kamsi.nwabueze@scsstudent.org>
2022-02-20 11:34:25 -0800

    added basics(index footer etc)

---
+++ note_taker/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from "./footer.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Never Scroll Through PDFs Again!</h1>
+      </header>
+      <Footer />  
+    </div>
+  );
+}
+
+export default App;
---
+++ note_taker/src/footer.js
@@ -0,0 +1,10 @@
+import './footer.css';
+const footer = () =>{
+    return(
+        <div className = "foot">
+            <footer>Developed by Kamsi Nwabueze, Vismay Ravikumar, and Andy Dieu </footer>
+        </div>
+        
+    );
+};
+export default footer;
