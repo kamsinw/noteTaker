92f4c97fa6cd44caa9566da74780c93fbe0e0d33
kam1202 <kamsi.nwabueze@scsstudent.org>
2022-02-20 14:00:30 -0800

    Update sidebar.js

--- note_taker/src/sidebar.js
+++ note_taker/src/sidebar.js
@@ -1,13 +1,21 @@
+import React,{useState} from 'react'
 import './sidebar.css';
+
 const sidebar = () =>{
+   
     const clickhandler = () =>{
         console.log("clicked")
     }
     return(
+        
         <div className = "border">
             <div>
-               <h3>Choose a Pdf</h3>
-               <button onClick = {clickhandler}> Choose</button>
+              <form className = "formGroup" >
+                <input type="file" className = 'form-control' required  />
+                <button type = "submit" className = "uplBtn">
+                        UPLOAD
+                </button>
+              </form>
             </div>
         </div>
     );
